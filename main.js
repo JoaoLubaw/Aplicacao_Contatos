c8db97a7de642c15c960692663a577715d5f79f4
João Lubawaski <jlubawaski@gmail.com>
2023-03-29 20:39:32 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,51 @@
+const form = document.getElementById('add');
+let linhas = '';
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    const inputNome = document.getElementById('nome');
+    const inputTelefone = document.getElementById('telefone');
+    
+    let linha = '<tr>';
+    linha += `<td>${inputNome.value}</td>`;
+    linha += `<td>${inputTelefone.value}</td>`;
+    linha += '</tr>';
+
+    linhas += linha;
+
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+
+    inputNome.value = '';
+    inputTelefone.value ='';
+})
+
+/* Máscaras ER - Código disponivel online */
+
+function mascara(o,f){
+    v_obj=o
+    v_fun=f
+    setTimeout("execmascara()",1)
+}
+function execmascara(){
+    v_obj.value=v_fun(v_obj.value)
+}
+function mtel(v){
+    v=v.replace(/\D/g,""); //Remove tudo o que não é dígito
+    v=v.replace(/^(\d{2})(\d)/g,"($1) $2"); //Coloca parênteses em volta dos dois primeiros dígitos
+    v=v.replace(/(\d)(\d{4})$/,"$1-$2"); //Coloca hífen entre o quarto e o quinto dígitos
+    return v;
+}
+function id( el ){
+	return document.getElementById( el );
+}
+window.onload = function(){
+	id('telefone').onkeyup = function(){
+		mascara( this, mtel );
+	}
+}
+
+/* Fim do código copiado, créditos para https://codepen.io/adelmodias*/
+
+
